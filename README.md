989266f9408dadd3a0d691d018bfb683470aabf6
sugarshin <shinsugar@gmail.com>
2018-08-05 18:45:21 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# authentication
+
+ReMap Authentication Microservice.
+
+Verify Firebase ID Tokens.
+
+## Authentication
+
+```http
+curl / -d '{"idToken": "... token"}'
+
+HTTP/1.1 200 OK
+Content-Type: application/json; charset=utf-8
+Content-Length: 224
+Date: Sun, 05 Aug 2018 09:33:41 GMT
+Connection: keep-alive
+
+{
+  "name": "Shingo Sato",
+  "picture": "https://graph.facebook.com/1736908143023349/picture",
+  "auth_time": 1533455916,
+  "email": "shinsugar@gmail.com",
+  "email_verified": true,
+  "uid": "sdfsd098f0sd98f"
+}
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const { send, json } = require('micro')
+const { router, get, post } = require('microrouter')
+const admin = require('firebase-admin')
+const statuses = require('statuses')
+const pick = require('lodash.pick')
+const serviceAccount = require('./service-account-key.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+module.exports = router(
+  get('*', (req, res) => send(res, 404, { error: statuses[404] })),
+  post ('/', async (req, res) => {
+    let idToken
+    try {
+      const body = await json(req)
+      idToken = body.idToken
+    } catch (e) {
+      send(res, 400, { error: statuses[400] })
+      return
+    }
+
+    if (!idToken) {
+      send(res, 400, { error: statuses[400] })
+      return
+    }
+
+    const decodedToken = await admin.auth().verifyIdToken(idToken)
+      .catch(() => send(res, 401, { error: statuses[401] }))
+
+    if (decodedToken) {
+      const responseBody = pick(decodedToken, [
+        'name',
+        'picture',
+        'auth_time',
+        'email',
+        'email_verified',
+        'uid',
+      ])
+      send(res, 200, responseBody)
+    }
+  })
+)
