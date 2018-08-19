0f997087e0b1a4fc8001fbba1b79975b8e9bd6ca
sugarshin <shinsugar@gmail.com>
2018-08-19 23:07:10 +0900

    Remove `statuses`

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const { STATUS_CODES } = require('http')
 const { send, json } = require('micro')
 const { router, get, post } = require('microrouter')
 const admin = require('firebase-admin')
-const statuses = require('statuses')
 const pick = require('lodash.pick')
 const serviceAccount = require('./service-account-key.json') // eslint-disable-line
 
@@ -10,24 +10,24 @@ admin.initializeApp({
 })
 
 module.exports = router(
-  get('*', (req, res) => send(res, 404, { error: statuses[404] })),
+  get('*', (req, res) => send(res, 404, { error: STATUS_CODES[404] })),
   post ('/', async (req, res) => {
     let idToken
     try {
       const body = await json(req)
       idToken = body.idToken
     } catch (e) {
-      send(res, 400, { error: statuses[400] })
+      send(res, 400, { error: STATUS_CODES[400] })
       return
     }
 
     if (!idToken) {
-      send(res, 400, { error: statuses[400] })
+      send(res, 400, { error: STATUS_CODES[400] })
       return
     }
 
     const decodedToken = await admin.auth().verifyIdToken(idToken)
-      .catch(() => send(res, 401, { error: statuses[401] }))
+      .catch(() => send(res, 401, { error: STATUS_CODES[401] }))
 
     if (decodedToken) {
       const responseBody = pick(decodedToken, [
